29e99554e8f9a4229419dea9a9e84b4cd40ab993
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2025-07-31 16:25:21 +0530

    did some ui ig (bs)

--- ui/window.py
+++ ui/window.py
@@ -1,6 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 import os
 import sys
+import psutil
+import threading
+import time
 
 def get_resource_path(relative_path):
     """ Get absolute path to resource (compatible with PyInstaller) """
@@ -12,13 +16,16 @@ def get_resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+
 root = tk.Tk()
 root.title("ThreadWeaver - Smart Task Manager")
 root.geometry("600x400")
+root.configure(bg="#e0d8d7")
+root.attributes("-alpha", 0.86)
 
+cpu_usage = tk.Label(root, text="CPU Usage",bg="#e0d8d7",fg="#0f172a",font=("Segoe UI",40,"bold")) #bg dekhlio kya krna hai lallu
+cpu_usage.pack()
 icon_path = get_resource_path("assets/icon.png")
 icon_img = tk.PhotoImage(file=icon_path)
 root.iconphoto(False, icon_img)
-
-
 root.mainloop()
